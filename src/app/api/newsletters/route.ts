4ef122f70974ee5f1c9f49c8de84b7370e5b97ed
loichuck <huck@lemonde.fr>
2024-04-28 14:31:23 +0200

    fix: force api route to be dynamic

--- src/app/api/newsletters/route.ts
+++ src/app/api/newsletters/route.ts
@@ -3,6 +3,8 @@ import { randomBetween } from "@/utils/random";
 import { wait } from "@/utils/wait";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   await wait(randomBetween(100, 500)); // Random delay to simulate calls
   return NextResponse.json(NEWSLETTER_ITEMS);
